e3890645fcbde9d5220800ecd0519ba6958d81c1
VillaviH <hsoledispa5@gmail.com>
2025-04-24 18:30:38 -0500

    update0_24abr25

--- opentofu/backend.tf
+++ opentofu/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "opentofu-state-demoflisol2025" # Reemplaza con un nombre único
+    bucket = "opentofu-state-demoflisol2025" # Reemplazar con un nombre único
     key    = "react-app/tofu.tfstate"
     region = "us-east-1" # Reemplaza con tu región de AWS
   }
